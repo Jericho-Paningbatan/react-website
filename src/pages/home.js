a081280e22c7c1def3a105380b3eada6bc1f27ac
Jericho Paningbatan <echoop058@gmail.com>
2024-01-09 23:06:50 +0800

    11:06 pm

--- src/pages/home.js
+++ src/pages/home.js
@@ -42,13 +42,18 @@ const home = () => {
 
 
           </div>
-          <div className='left-intro animate1'>
+          <div className='left-intro'>
+            <div className='left-intro1 animate1'>
               <h5>Hi,</h5>
               <h2>I'm <span>Jericho Paningbatan</span></h2>
               <h3>Front-End Developer</h3>
-              <p className= 'p-first'>With a creative edge and a focus on crafting visually appealing and intuitive user interfaces.</p>
+            </div>
+              <div className='left-intro2 animate2'>
+                <p className= 'p-first'>With a creative edge and a focus on crafting visually appealing and intuitive user interfaces.</p>
                 <p className= 'p-second'> I specialize in transforming ideas into responsive, dynamic websites, blending my technical 
                 <br></br>expertise with a keen eye for design. Let's build captivating digital experiences together!</p>
+              </div>
+            
 
                 <div className='buttons animate3'>
                   <div className='button1'>Hire Me</div>
